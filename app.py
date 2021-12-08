9a05c451572573ceefd8b13578e251198acce635
uday7095 <87174745+uday7095@users.noreply.github.com>
2021-12-08 21:02:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# list of cat images
+images = [
+    "http://scratchpads.eu/explore/sites-list"
+]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
